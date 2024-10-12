78d273fdb6bad7c881ccc6dd6ae5efa04edb3ab6
xinbaojian <baojian.xin.py@gmail.com>
2024-10-12 09:36:16 +0800

    Remove unused imports to clean up the main module in Rust.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 #[macro_use]
 extern crate rocket;
+mod api;
 mod consul;
 mod entity;
 mod utils;
-mod api;
 
-use crate::entity::MarkdownMessage;
 use api::*;
 use rocket::log::private::{log, Level};
-use rocket::serde::json::{serde_json, Json};
-use rocket::Request;
-use utils::http_utils;
 
 #[launch]
 async fn rocket() -> _ {
