d4fbee76ceebffacc3509cab43d984e6ff406bef
xinbaojian <baojian.xin.py@gmail.com>
2024-10-12 16:30:39 +0800

    添加日志记录和参数校验以增强消息发送功能。

--- src/api/message.rs
+++ src/api/message.rs
@@ -1,6 +1,7 @@
 use crate::entity;
 use crate::entity::{MarkdownMessage, MessageSend};
 use crate::utils::http_utils;
+use rocket::log::private::{log, Level};
 use rocket::serde::json::{serde_json, Json};
 
 /// 发送消息
@@ -10,7 +11,11 @@ use rocket::serde::json::{serde_json, Json};
 /// 返回消息内容
 #[post("/message/send/<key>", data = "<message>")]
 pub async fn message_send(key: String, message: Json<MessageSend>) -> String {
-    println!("{:?}", message);
+    // 校验参数是否有值
+    if message.subject.is_empty() || message.content.is_empty() {
+        return "参数有误".to_string();
+    }
+    log!(Level::Info, "message: {:?}", message);
     let markdown_message = MarkdownMessage::from_message_send(&message);
     http_utils::post(
         format!("{}{}", entity::WECOM_BOT_URL, key).as_str(),
